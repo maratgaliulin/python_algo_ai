e383eb4ceb59b05fe1b72c9183af7f3e78c249eb
maratgaliulin <egomarat@gmail.com>
2025-02-18 02:41:54 +1100

    .

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from methods.make_time_series_data import make_time_series_data
+from methods.return_single_large_dataframe import return_single_large_dataframe
 
 
-make_time_series_data('hist_data/', '1_min', '5_min', '5min')
+# make_time_series_data('hist_data/', '1_min', '5_min', '5min')
+
+return_single_large_dataframe('hist_data/EURUSD/', '5_min/', 'Bid')
