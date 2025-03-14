9246ddc333c2e332f871460e56feaf3c24628dcd
maratgaliulin <egomarat@gmail.com>
2025-03-14 11:15:00 +1100

    .

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from methods.draw_graph import draw_static_graph
 from methods.make_single_df_from_bid_ask import make_single_df_from_bid_ask
+from methods.return_single_large_dataframe import return_single_large_dataframe
 
 base_dir = "hist_data/EURUSD/5_min/"
 
@@ -16,13 +17,12 @@ rename_columns_dict = {
     'Volume': 'volume'
 }
 
-df_5min_bid = pd.read_csv(dir_bid, index_col="Gmt time").sort_index(ascending=True)
-df_5min_ask = pd.read_csv(dir_ask, index_col="Gmt time").sort_index(ascending=True)
-df_5min_bid.rename(columns=rename_columns_dict, inplace=True)
-df_5min_ask.rename(columns=rename_columns_dict, inplace=True)
+# df_5min_bid = pd.read_csv(dir_bid, index_col="Gmt time").sort_index(ascending=True)
+# df_5min_ask = pd.read_csv(dir_ask, index_col="Gmt time").sort_index(ascending=True)
 
 
-df_5min_joined = make_single_df_from_bid_ask(df_5min_bid, df_5min_ask)
+
+
 
 # print(df_5min_bid.tail(50))
 # print('********************')
@@ -31,4 +31,21 @@ df_5min_joined = make_single_df_from_bid_ask(df_5min_bid, df_5min_ask)
 # for i in range(0,90):
 #     print(df_5min_joined.iloc[i].T)
 
-draw_static_graph(df_5min_bid.tail(70), df_5min_ask.tail(70), df_5min_joined.tail(70))
+# draw_static_graph(df_5min_bid.tail(70), df_5min_ask.tail(70), df_5min_joined.tail(70))
+
+bdir = "hist_data/"
+time_series_folder = "EURUSD/5_min/"
+bid_or_ask_folder_bid = "Bid/"
+bid_or_ask_folder_ask = "Ask/"
+
+df_5min_bid = return_single_large_dataframe(base_dir=bdir, time_series_folder=time_series_folder, Bid_or_Ask_folder=bid_or_ask_folder_bid)
+df_5min_ask = return_single_large_dataframe(base_dir=bdir, time_series_folder=time_series_folder, Bid_or_Ask_folder=bid_or_ask_folder_ask)
+
+df_5min_bid.rename(columns=rename_columns_dict, inplace=True)
+df_5min_ask.rename(columns=rename_columns_dict, inplace=True)
+
+df_5min_joined = make_single_df_from_bid_ask(df_5min_bid, df_5min_ask)
+
+print(df_5min_joined.head(20))
+print(df_5min_joined.tail(20))
+print(len(df_5min_joined))
--- methods/return_single_large_dataframe.py
+++ methods/return_single_large_dataframe.py
@@ -5,7 +5,10 @@ def return_single_large_dataframe(base_dir:str, time_series_folder:str, Bid_or_A
     total_dir = base_dir + time_series_folder + Bid_or_Ask_folder
     columns = ['Gmt time', 'Open', 'High', 'Low', 'Close', 'Volume']
     final_df = pd.DataFrame(columns=columns)
+        
     for file in os.listdir(total_dir):
-        print(file)
+        final_dir = total_dir + file
+        new_df = pd.read_csv(final_dir, index_col="Gmt time").sort_index(ascending=True)
+        final_df = pd.concat([final_df, new_df])
 
-    print(final_df)
+    return final_df
