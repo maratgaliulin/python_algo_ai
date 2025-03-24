1ad54374f3d06d6a64ef218d801ccbfb207d8955
maratgaliulin <egomarat@gmail.com>
2025-03-25 09:56:01 +1100

    .

--- eurusd_loop.py
+++ eurusd_loop.py
@@ -1,11 +1,56 @@
 from credentials import *
 import time
 import pandas as pd
+import numpy as np
 from methods.make_dataframe_line import make_dataframe_line
 from machine_learning_models.random_forest.eurusd.random_forest_algorithm import random_forest_algorithm
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
+
+def buy_or_sell(
+    dataframe_line:pd.DataFrame,
+    min_impulse_size: float,
+    orders_of_the_symbol:tuple,
+    positions_of_the_symbol: tuple,
+    present_price_bid:float,
+    present_price_ask:float,
+    point:float,
+    order_type_buy,
+    order_type_sell,
+    order_action,
+    lot:float
+                ) -> None:
+    
+    entry, sl, tp = 0, 0, 0    
+    high_value, low_value, trend_direction = random_forest_algorithm(dataframe_line=dataframe_line,
+                                                                     pickle_rfc_predict_max_dir=eurusd_dict['BASE_DIR'] + eurusd_dict['EURUSD_RFR_MAX_VAL_AFTER_TEST_TR'],
+                                                                     pickle_rfc_predict_min_dir=eurusd_dict['BASE_DIR'] + eurusd_dict['EURUSD_RFR_MIN_VAL_AFTER_TEST_TR'],
+                                                                     pickle_rfc_predict_trend_dir=eurusd_dict['BASE_DIR'] + eurusd_dict['EURUSD_RFR_TREND_DIR_BEFORE_TEST_TR']
+                                                                     )
+    trend_direction = trend_direction.tolist()
+    actual_impulse_size = high_value - low_value
+    
+    if(trend_direction[0] == [1, 0, 0]):
+        trend_direction_string = 'downtrend'
+    elif(trend_direction[0] == [0, 1, 0]):
+        trend_direction_string = 'uptrend'
+    elif(trend_direction[0] == [0, 0, 1]):
+        trend_direction_string = 'undefined'
+    else:
+        trend_direction_string = 'error'
+    
+    impulse_satisfies_minimal_size = (actual_impulse_size >= min_impulse_size)
+    orders_of_the_symbol_is_null = orders_of_the_symbol == ()
+    positions_of_the_symbol_is_null = positions_of_the_symbol == ()
+    trend_is_predicted = ((trend_direction_string == 'uptrend') or (trend_direction_string == 'downtrend'))
+    
+    print(f'Impulse satisfies minimal_size: {impulse_satisfies_minimal_size[0]}')
+    print(f'There are no open orders: {orders_of_the_symbol_is_null}')
+    print(f'There are no open positions: {positions_of_the_symbol_is_null}')
+    print(f'Trend is predicted: {trend_is_predicted}. Actual trend direction: {trend_direction_string}')
+    
+    
 
 while True:
     
@@ -52,14 +97,21 @@ while True:
                                          )
     # print(dataframe_line)
     
-    high_value, low_value, trend_direction = random_forest_algorithm(dataframe_line=dataframe_line,
-                                                                     pickle_rfc_predict_max_dir=eurusd_dict['BASE_DIR'] + eurusd_dict['EURUSD_RFR_MAX_VAL_AFTER_TEST_TR'],
-                                                                     pickle_rfc_predict_min_dir=eurusd_dict['BASE_DIR'] + eurusd_dict['EURUSD_RFR_MIN_VAL_AFTER_TEST_TR'],
-                                                                     pickle_rfc_predict_trend_dir=eurusd_dict['BASE_DIR'] + eurusd_dict['EURUSD_RFR_TREND_DIR_BEFORE_TEST_TR']
-                                                                     )
+    # 'y_trend_downtrend', 'y_trend_uptrend', 'y_trend_trend undefined'
     
-    print(high_value, low_value)
-    print(trend_direction)
+    buy_or_sell(
+        dataframe_line=dataframe_line,
+        min_impulse_size=0.0025,
+        orders_of_the_symbol=eurusd_dict['ORDERS_OF_THE_SYMBOL'],
+        positions_of_the_symbol=eurusd_dict['POSITIONS_OF_THE_SYMBOL'],
+        present_price_bid=eurusd_dict['PRESENT_PRICE_BID'],
+        present_price_ask=eurusd_dict['PRESENT_PRICE_ASK'],
+        point=eurusd_dict['POINT'],
+        lot=eurusd_dict['LOT'],
+        order_type_buy=eurusd_dict['ORDER_TYPE_BUY'],
+        order_type_sell=eurusd_dict['ORDER_TYPE_SELL'],
+        order_action=eurusd_dict['ORDER_ACTION']
+    )
 
 
     
